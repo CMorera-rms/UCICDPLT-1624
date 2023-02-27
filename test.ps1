d1a69e7cdfa21129ef6da8535a1ea06b9c45f7d9
Catalina Morera <cmorera@rms.com>
2023-02-27 06:55:09 -0800

    Add parameters file

--- test.ps1
+++ test.ps1
@@ -1,9 +1,2 @@
 
-#Import-Module -Name ActiveDirectory
-
-#Get-ADUser -Identity cmorera -Properties *
-
-#$currentSnowflakeUsers = snowsql -c okta -q 'SELECT TOP 10 * FROM WEATHER_14_TOTAL'
-#write-host $currentSnowflakeUsers | convertfrom-csv
-
 ./release_generator
