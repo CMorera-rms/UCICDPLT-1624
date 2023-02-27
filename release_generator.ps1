63909cb164e235b51a1fc7d1800bee685bdf9989
Catalina Morera <cmorera@rms.com>
2023-02-27 05:57:49 -0800

    Create script template

---
+++ release_generator.ps1
@@ -0,0 +1,21 @@
+### Functions ###
+#param ([String[]]  $usersList)
+
+
+
+function Read-ReleaseTemplateData {
+    write-host "ReadFile"
+}
+
+function Convert-ReleaseTemplateDataJSON {
+    write-host "Convert File"
+}
+
+function New-ReleasePipeline {
+    write-host "Create Release Pipeline"
+}
+
+
+Read-ReleaseTemplateData
+Convert-ReleaseTemplateDataJSON
+New-ReleasePipeline
---
+++ test.ps1
@@ -0,0 +1,9 @@
+
+#Import-Module -Name ActiveDirectory
+
+#Get-ADUser -Identity cmorera -Properties *
+
+#$currentSnowflakeUsers = snowsql -c okta -q 'SELECT TOP 10 * FROM WEATHER_14_TOTAL'
+#write-host $currentSnowflakeUsers | convertfrom-csv
+
+./release_generator
