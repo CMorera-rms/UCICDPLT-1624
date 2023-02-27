d72b4033a291f87bb8fb62d21e40766a3905efc7
Catalina Morera <cmorera@rms.com>
2023-02-27 08:24:03 -0800

    Add logic to read json file data

--- release_generator.ps1
+++ release_generator.ps1
@@ -4,14 +4,22 @@
 
 
 function Read-ReleaseTemplateData {
-    write-host "ReadFile"
+    write-host "-----------------------------------------------------------"
+    write-host "Read parameters"
+    $json = Get-Content -Raw -Path parameters.json | ConvertFrom-Json
+    write-host $json.sourceId
+    write-host $json.createdBy
+    write-host $json.modifiedBy
+
 }
 
 function Convert-ReleaseTemplateDataJSON {
+    write-host "-----------------------------------------------------------"
     write-host "Convert File"
 }
 
 function New-ReleasePipeline {
+    write-host "-----------------------------------------------------------"
     write-host "Create Release Pipeline"
 }
 
