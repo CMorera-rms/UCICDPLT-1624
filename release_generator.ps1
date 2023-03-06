85575e612bbf087fccd835d776af1ae6855bb9d0
Catalina Morera <cmorera@rms.com>
2023-03-06 10:49:12 -0800

    Add template file

--- release_generator.ps1
+++ release_generator.ps1
@@ -1,46 +1,119 @@
 ### Functions ###
 #param ([String[]]  $usersList)
 
+$Organization = "cmorera"
+
+$ProjectId = "3b0727f4-d94e-433c-ad69-9216c50ea468"
+$ProjectName = "cata-test"
+
+$Token = "kttwvkvlaojpfirum4nvzcqnwduw6hvkyueqdzmncbiabldq5hta"
+$User = ''
+$Pass = $Token
+$Headers = @{ Authorization = "Basic "+ [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($User+":"+$Pass)) }
 
 
-function Read-ReleaseTemplateData {
+function New-ComposeJsonBodyOptions {
     write-host "-----------------------------------------------------------"
-    write-host "Read parameters"
-    $json = Get-Content -Raw -Path test.json | ConvertFrom-Json
-    return $json
+    write-host "Compose request body JSON"
+
+    $Json = Get-Content -Raw -Path template.json | ConvertFrom-Json 
+    
+    #BuildDefinition id should be summited
+    $BuildDefinitionId = 7
+    #write-host $Json
+    New-ComposeArtifactsJson -BuildDefinitionId $BuildDefinitionId -JsonBody $Json
+
+    return $Json
 }
 
-function Convert-ReleaseTemplateDataJSON {
-    write-host "-----------------------------------------------------------"
-    write-host "Convert File"
-    $json = Get-Content -Raw -Path test.json | ConvertFrom-Json  | ConvertTo-Json -Depth 9
-    return $json
+function New-ComposeArtifactsJson {
+    param (
+        [parameter(Mandatory = $false)]
+        [int]$BuildDefinitionId = -1,
+
+        [parameter(Mandatory = $false)]
+        [PSObject] $JsonBody
+    )
+    
+    #Get the lkg build id according to the build definition id
+    #TODO: Do functionality to get the lkg id
+    #write-host $BuildDefinitionId
+    $BuildDefinitionName = "webapi-manual-pipeline-build"
+    $BuildLKG = 30
+    $BuildId = $BuildLKG
+   
+    #Get artifact data from AzureDevOps API according to the lkg build id
+    $Url = "https://dev.azure.com/"+$Organization+"/"+$ProjectName+"/_apis/build/builds/"+$BuildId+"/artifacts?api-version=4.1"
+    $ArtifactData = Invoke-RestMethod  -Uri $Url  -Method Get  -Headers $Headers
+    #write-host $ArtifactData.value
+
+    $ArtifactId = $ArtifactData.value.id
+    $ArtifactName = $ArtifactData.value.name
+
+    #write-host $ArtifactId
+    #write-host $ArtifactName
+
+    write-host $JsonBody.artifacts.alias
+
+    #Update values on JSON body template
+    $JsonBody.artifacts.alias1 = $ArtifactName
+
+    #Update defaultVersionSpecific data
+    #write-host $JsonBody.artifacts.definitionReference.defaultVersionSpecific
+    $JsonBody.artifacts.definitionReference.defaultVersionSpecific.id = $ArtifactId
+    $JsonBody.artifacts.definitionReference.defaultVersionSpecific.name = $ArtifactName
+    #write-host $JsonBody.artifacts.definitionReference.defaultVersionSpecific
+
+    #Update definition data
+    #write-host $JsonBody.artifacts.definitionReference.definition
+    $JsonBody.artifacts.definitionReference.definitions.id = $BuildDefinitionId
+    $JsonBody.artifacts.definitionReference.definitions.name = $BuildDefinitionName
+    #write-host $JsonBody.artifacts.definitionReference.definition
+
+    #Update project data
+    #write-host $JsonBody.artifacts.definitionReference.project
+    $JsonBody.artifacts.definitionReference.project.id = $ProjectId
+    $JsonBody.artifacts.definitionReference.project.name = $ProjectName
+    #write-host $JsonBody.artifacts.definitionReference.project
+
 }
 
-function New-ReleasePipeline {
+function New-ComposeEnviromentsJson {
+    param (
+    
+        [parameter(Mandatory = $false)]
+        [PSObject] $JsonBody
+    )
+   write-host $JsonBody.source
+}
+
+function New-ComposeVariablesJson{
+    param (
+        [parameter(Mandatory = $false)]
+        [PSObject] $JsonBody
+    )
+
+   write-host $JsonBody.source
+}
+
+
+function New-CreateReleasePipeline {
     write-host "-----------------------------------------------------------"
-    write-host "Create Release Pipeline"
-    $organization = "cmorera"
-    $project = "cata-test"
-    $token = "5xp76tq5zjetjfj4dj5jsilvuxffr6mxhebt6nidbngu6km4sufa"
-    $url = "https://vsrm.dev.azure.com/"+$organization+"/"+$project+"/_apis/release/definitions?api-version=7.1-preview.4"
-    write-host $url 
+    $JsonBody =  New-ComposeJsonBodyOptions | ConvertTo-Json -Depth 9
 
-    $user = ''
-    $pass = $token
-    $headers = @{ Authorization = "Basic "+ [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($user+":"+$pass)) }
-    $body =  Convert-ReleaseTemplateDataJSON
-    write-host $body.source
+    #$Json = Get-Content -Raw -Path template.json | ConvertFrom-Json  | ConvertTo-Json
 
+    #Write-Host ($JsonBody.artifacts.definitionReference.defaultVersionSpecific)
+    #Write-Host ($JsonBody.artifacts.definitionReference.definition )
+    #Write-Host ($JsonBody.artifacts.definitionReference.project )
 
-    $JsonBody = $body
+    #write-host "Do Create Release Pipeline Request"
+    $url = "https://vsrm.dev.azure.com/"+$Organization+"/"+$ProjectName+"/_apis/release/definitions?api-version=7.1-preview.4"
+    #write-host $url 
     
     $result = Invoke-RestMethod -ContentType "application/json" -Uri $url  -Method Post  -Body $JsonBody  -Headers $headers
-    write-host $result
-
+    write-host $result 
+    
 }
 
-
-#Read-ReleaseTemplateData
-#Convert-ReleaseTemplateDataJSON
-New-ReleasePipeline
+New-CreateReleasePipeline
